8a2c8f06eab31b31dea63ff43a13037f6df13018
klueless-io <david@ideasmen.com.au>
2021-04-28 19:35:55 +1000

    initial setup

---
+++ Context/DbPgContext.cs
@@ -0,0 +1,17 @@
+﻿namespace P12Location.Context
+{
+  using Microsoft.EntityFrameworkCore;
+  using P12Location.Models;
+
+  public class DbPgContext : DbContext
+  {
+    public DbPgContext(DbContextOptions<DbPgContext> options) : base(options)
+    {
+    }
+    public DbSet<Location> Locations { get; set; }
+
+  
+    // protected override void OnConfiguring(DbContextOptionsBuilder options)
+    //         => options.UseNpgsql("Host=127.0.0.1;Port=5432;Database=P12;Username=postgres;Password=Hambaro3");
+  }
+}
---
+++ Controllers/LocationController.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using P12Location.Context;
+using P12Location.Models;
+
+namespace P12Location.Controllers
+{
+    public class LocationController : Controller
+    {
+        private readonly ILogger<LocationController> _logger;
+
+        private readonly DbPgContext _context;
+
+        public LocationController(DbPgContext context, ILogger<LocationController> logger)
+        {
+            _logger = logger;
+            _context = context;
+        }
+
+        // GET: Location 
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Locations.ToListAsync());
+        }
+
+        // GET: Location/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var Location = await _context.Locations
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (Location == null)
+            {
+                return NotFound();
+            }
+
+            return View(Location);
+        }
+
+        // GET: Location/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Location/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name,Latitude,Longitude,Active")] Location location)
+        {
+          try
+          {
+            if (ModelState.IsValid)
+            {
+                _context.Add(location);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+          }
+          catch(Exception ex)
+          {
+            ModelState.AddModelError("Error", "Oops there was an error, please try again");
+            // throw 
+          }
+          Console.WriteLine(ModelState.ErrorCount);
+          return View(location);
+        }
+
+        // GET: Location/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var Location = await _context.Locations.FindAsync(id);
+            if (Location == null)
+            {
+                return NotFound();
+            }
+            return View(Location);
+        }
+
+        // POST: Location/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Latitude,Longitude,Active")] Location Location)
+        {
+            if (id != Location.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(Location);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!LocationExists(Location.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(Location);
+        }
+
+        // GET: Location/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var Location = await _context.Locations
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (Location == null)
+            {
+                return NotFound();
+            }
+
+            return View(Location);
+        }
+
+        // POST: Location/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var Location = await _context.Locations.FindAsync(id);
+            _context.Locations.Remove(Location);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool LocationExists(int id)
+        {
+            return _context.Locations.Any(e => e.Id == id);
+        }
+    }
+}
